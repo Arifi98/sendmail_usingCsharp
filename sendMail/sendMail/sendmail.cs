e392cb45249f7515c2bc9bd3df9db0151d8bdd2e
arif.bucpapaj@fshnstudent.info <arif.bucpapaj@fshnstudent.info>
2020-01-21 16:57:41 +0100

    first commit

---
+++ sendMail/sendMail/sendmail.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+
+namespace sendMail
+{
+    public class sendmail
+    {
+        protected void send()
+        {
+            string emailTo = "eduarthajko1992@gmail.com";
+            string emailFrom = "arifbucpapaj98@gmail.com";
+            string emailCc = "asdf@asdf.asdf";
+            string emailSubject = "asdf";
+            string emailBody = "asdff test asdf";
+            System.Net.Mail.MailMessage mail = new System.Net.Mail.MailMessage();
+            SmtpClient SmtpServer = new SmtpClient("smtp.sefin.it");
+
+
+            mail.From = new MailAddress(emailFrom);
+
+            mail.To.Add(emailTo);
+
+            mail.Subject = emailSubject;
+            mail.Body = emailBody;
+
+            //SmtpServer.Port = ?;
+            SmtpServer.Credentials = new System.Net.NetworkCredential("esmerald.aliaj@sefin.it", "Complex123");
+            //SmtpServer.EnableSsl = true; // Sefin's smtp does not support ssl.
+
+            try
+            {
+                SmtpServer.Send(mail);
+                //to fix file lock when it has attachments
+                mail.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Caught: {0}", ex.Message);
+                if (ex.InnerException != null)
+                    Console.WriteLine("Inner exception: {0}", ex.InnerException);
+            }
+        }
+    }
+
+}
+}
